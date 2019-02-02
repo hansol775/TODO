f673e80c8e52ec81bb1a517fcb23d6d9bf700300
hansol775 <axscdv0@naver.com>
2019-02-02 19:18:41 +0900

    gg

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <!-- HTML 태그 내용 -->
+    <!-- 화면에 표시할 요소들을 정의하는 영역, HTML+뷰 데이터 바인딩 -->
+    <div>
+        <h1>할일 앱</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        // 자바 스크립트 내용
+        // 뷰 컴포넌트의 내용을 정의하는 영역, template, data, methods 등
+        // export default {} 는 ES6의 자바스크립트 모듈화와 관련된 문법.
+    }
+</script>
+
+<style scoped>
+    /* CSS 스타일 내용 */
+    /* 템플릿에 추가한 HTML 태그의 CSS 스타일을 정의하는 영역 */
+        h1 {
+        color : blue;
+        font-weight : 900;
+        margin : 2.5rem 0 1.5rem;
+    }
+</style>
